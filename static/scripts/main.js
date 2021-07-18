1e2a6a6b56bed3bc391f46c614d912fc5314c28c
castelanlan <gabrielmcastelan@gmail.com>
2021-07-17 22:50:52 -0300

    Começo do trocador de imagens, não funcional

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,4 +1,15 @@
+function aleatorio_limite(limite) {
+    return Math.floor(Math.random() * (limite + 1)) // 0..limite
+}
+
 function recarregar_pagina() {
-    console.log("funfo")
-    window.location.href = window.location.href
+    window.location.href = window.location.href; // supostamente recarrega o cache junto com a página
+    console.log("Página recarregada");
+}
+
+function pepe_aleatorio() {
+    var pepe = `assets/pepes/pepe_${aleatorio_limite(5)} `;
+    console.log(pepe)
+    document.getElementsByClassName('pepe').src = pepe
+    return pepe
 }
